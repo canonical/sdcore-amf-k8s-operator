375941f998378cd83703306233068a1ed061b908
yazansalti <yazan.salti@canonical.com>
2023-11-15 10:30:28 +0400

    Addresses review comments

--- src/charm.py
+++ src/charm.py
@@ -242,9 +242,10 @@ class AMFOperatorCharm(CharmBase):
         if not self._private_key_is_stored():
             event.defer()
             return
-        if not self._certificate_is_stored():
-            self._request_new_certificate()
+        if self._certificate_is_stored():
             return
+
+        self._request_new_certificate()
 
     def _on_certificate_available(self, event: CertificateAvailableEvent) -> None:
         """Pushes certificate to workload and configures AMF."""
