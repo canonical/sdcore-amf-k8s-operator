7c061b194a78b0f497f1d056163d314d8adfe734
Bartlomiej Gmerek <bartlomiej.gmerek@canonical.com>
2024-02-12 09:30:37 +0100

    Updating base Terraform module

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,55 +1,15 @@
+# Copyright 2024 Canonical Ltd.
+# See LICENSE file for licensing details.
+
 resource "juju_application" "amf" {
-  name  = "amf"
+  name  = var.app_name
   model = var.model_name
 
   charm {
     name    = "sdcore-amf-k8s"
     channel = var.channel
   }
-  config = var.amf-config
+  config = var.config
   units  = 1
   trust  = true
 }
-
-resource "juju_integration" "amf-db" {
-  model = var.model_name
-
-  application {
-    name     = juju_application.amf.name
-    endpoint = "database"
-  }
-
-  application {
-    name     = var.db_application_name
-    endpoint = "database"
-  }
-}
-
-resource "juju_integration" "amf-certs" {
-  model = var.model_name
-
-  application {
-    name     = juju_application.amf.name
-    endpoint = "certificates"
-  }
-
-  application {
-    name     = var.certs_application_name
-    endpoint = "certificates"
-  }
-}
-
-resource "juju_integration" "amf-nrf" {
-  model = var.model_name
-
-  application {
-    name     = juju_application.amf.name
-    endpoint = "fiveg-nrf"
-  }
-
-  application {
-    name     = var.nrf_application_name
-    endpoint = "fiveg-nrf"
-  }
-}
-
