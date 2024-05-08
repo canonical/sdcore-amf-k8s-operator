e68c15bed524b0b353cdf20c1d5b4f60c5b9e8b7
Dario Faccin <dario.faccin@canonical.com>
2024-05-08 11:26:25 +0200

    feat: remove mandatory mongodb relation

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -63,7 +63,6 @@ async def test_relate_and_wait_for_active_status(ops_test: OpsTest, deploy):
         relation1=f"{NRF_CHARM_NAME}:database", relation2=f"{DB_CHARM_NAME}"
     )
     await ops_test.model.integrate(relation1=NRF_CHARM_NAME, relation2=TLS_PROVIDER_CHARM_NAME)
-    await ops_test.model.integrate(relation1=f"{APP_NAME}:database", relation2=f"{DB_CHARM_NAME}")
     await ops_test.model.integrate(relation1=APP_NAME, relation2=NRF_CHARM_NAME)
     await ops_test.model.integrate(relation1=APP_NAME, relation2=TLS_PROVIDER_CHARM_NAME)
     await ops_test.model.integrate(
@@ -114,20 +113,10 @@ async def test_restore_tls_and_wait_for_active_status(ops_test: OpsTest, deploy)
     reason="Bug in MongoDB: https://github.com/canonical/mongodb-k8s-operator/issues/218"
 )
 @pytest.mark.abort_on_fail
-async def test_remove_database_and_wait_for_blocked_status(ops_test: OpsTest, deploy):
-    assert ops_test.model
-    await ops_test.model.remove_application(DB_CHARM_NAME, block_until_done=True)
-    await ops_test.model.wait_for_idle(apps=[APP_NAME], status="blocked", timeout=TIMEOUT)
-
-
-@pytest.mark.skip(
-    reason="Bug in MongoDB: https://github.com/canonical/mongodb-k8s-operator/issues/218"
-)
-@pytest.mark.abort_on_fail
-async def test_restore_database_and_wait_for_active_status(ops_test: OpsTest, deploy):
+async def test_integrate_database_and_wait_for_active_status(ops_test: OpsTest, deploy):
     assert ops_test.model
     await _deploy_mongodb(ops_test)
-    await ops_test.model.integrate(relation1=APP_NAME, relation2=DB_CHARM_NAME)
+    await ops_test.model.integrate(relation1=f"{APP_NAME}:database", relation2=f"{DB_CHARM_NAME}")
     await ops_test.model.wait_for_idle(apps=[APP_NAME], status="active", timeout=TIMEOUT)
 
 
