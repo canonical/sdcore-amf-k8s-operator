28c7bfaad871917fade3f0eb4861d298cb1d82d2
Guillaume Belanger <guillaume.belanger@canonical.com>
2024-08-26 07:14:38 -0400

    chore: centralise fixtures and add scenario tests for integration (#366)
    * chore: centralize fixtures
    
    Signed-off-by: guillaume <guillaume.belanger27@gmail.com>
    
    * chore: use scenario for interface tests
    
    Signed-off-by: guillaume <guillaume.belanger27@gmail.com>
    
    * chore: freeze rdps-py
    
    Signed-off-by: guillaume <guillaume.belanger27@gmail.com>
    
    ---------
    
    Signed-off-by: guillaume <guillaume.belanger27@gmail.com>

--- tests/unit/lib/charms/sdcore_amf/v0/test_fiveg_n2_requirer_interface.py
+++ tests/unit/lib/charms/sdcore_amf/v0/test_fiveg_n2_requirer_interface.py
@@ -1,29 +1,12 @@
-# Copyright 2023 Canonical Ltd.
+# Copyright 2024 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-import logging
-import unittest
-from unittest.mock import call, patch
 
-from ops import testing
-from ops.charm import CharmBase
+import pytest
+import scenario
+from ops.charm import ActionEvent, CharmBase
 
 from lib.charms.sdcore_amf_k8s.v0.fiveg_n2 import N2InformationAvailableEvent, N2Requires
-
-METADATA = """
-name: fiveg-n2-requirer
-description: |
-  Dummy charm implementing the requirer side of the fiveg_n2 interface.
-summary: |
-  Dummy charm implementing the requirer side of the fiveg_n2 interface.
-requires:
-  fiveg-n2:
-    interface: fiveg-n2
-"""
-
-logger = logging.getLogger(__name__)
-
-CHARM_LIB_PATH = "lib.charms.sdcore_amf_k8s.v0.fiveg_n2"
 
 
 class DummyFivegN2Requires(CharmBase):
@@ -33,194 +16,121 @@ class DummyFivegN2Requires(CharmBase):
         super().__init__(*args)
         self.n2_requirer = N2Requires(self, "fiveg-n2")
         self.framework.observe(
-            self.n2_requirer.on.n2_information_available, self._on_n2_information_available
+            self.on.get_n2_information_action, self._on_get_n2_information_action
         )
 
-    def _on_n2_information_available(self, event: N2InformationAvailableEvent):
-        logger.info("N2 data, amf_ip_address: %s", event.amf_ip_address)
-        logger.info("N2 data, amf_hostname: %s", event.amf_hostname)
-        logger.info("N2 data, amf_port: %s", event.amf_port)
-
-
-class TestFiveGNRFRequirer(unittest.TestCase):
-    def setUp(self):
-        self.relation_name = "fiveg-n2"
-        self.remote_app_name = "dummy-n2-requirer"
-        self.remote_unit_name = f"{self.remote_app_name}/0"
-        self.harness = testing.Harness(DummyFivegN2Requires, meta=METADATA)
-        self.addCleanup(self.harness.cleanup)
-        self.harness.begin()
-
-    def _create_relation(self, remote_app_name: str):
-        relation_id = self.harness.add_relation(
-            relation_name=self.relation_name, remote_app=remote_app_name
-        )
-        self.harness.add_relation_unit(
-            relation_id=relation_id, remote_unit_name=f"{remote_app_name}/0"
+    def _on_get_n2_information_action(self, event: ActionEvent):
+        event.set_results(
+            results={
+                "amf-ip-address": self.n2_requirer.amf_ip_address,
+                "amf-hostname": self.n2_requirer.amf_hostname,
+                "amf-port": str(self.n2_requirer.amf_port),
+            }
         )
 
-        return relation_id
 
-    @patch(
-        f"{CHARM_LIB_PATH}.N2RequirerCharmEvents.n2_information_available",
-    )
+class TestFiveGNRFRequirer:
+    @pytest.fixture(autouse=True)
+    def context(self):
+        self.ctx = scenario.Context(
+            charm_type=DummyFivegN2Requires,
+            meta={
+                "name": "n2-requirer-charm",
+                "requires": {"fiveg-n2": {"interface": "fiveg_n2"}},
+            },
+            actions={
+                "get-n2-information": {},
+            },
+        )
+
     def test_given_n2_information_in_relation_data_when_relation_changed_then_n2_information_available_event_emitted(  # noqa: E501
         self,
-        patch_n2_information_available,
     ):
-        relation_id = self._create_relation(remote_app_name=self.remote_app_name)
-
-        relation_data = {
-            "amf_ip_address": "192.168.70.132",
-            "amf_hostname": "amf",
-            "amf_port": "38412",
-        }
-        self.harness.update_relation_data(
-            relation_id=relation_id, app_or_unit=self.remote_app_name, key_values=relation_data
+        fiveg_n2_relation = scenario.Relation(
+            endpoint="fiveg-n2",
+            interface="fiveg_n2",
+            remote_app_data={
+                "amf_ip_address": "192.168.70.132",
+                "amf_hostname": "amf",
+                "amf_port": "38412",
+            },
         )
-        calls = [
-            call.emit(
-                amf_ip_address="192.168.70.132",
-                amf_hostname="amf",
-                amf_port="38412",
-            ),
-        ]
-        patch_n2_information_available.assert_has_calls(calls, any_order=True)
+        state_in = scenario.State(
+            leader=True,
+            relations=[fiveg_n2_relation],
+        )
 
-    @patch(
-        f"{CHARM_LIB_PATH}.N2RequirerCharmEvents.n2_information_available",
-    )
+        self.ctx.run(fiveg_n2_relation.changed_event, state_in)
+
+        assert len(self.ctx.emitted_events) == 2
+        assert isinstance(self.ctx.emitted_events[1], N2InformationAvailableEvent)
+        assert self.ctx.emitted_events[1].amf_ip_address == "192.168.70.132"
+        assert self.ctx.emitted_events[1].amf_hostname == "amf"
+        assert self.ctx.emitted_events[1].amf_port == "38412"
+
     def test_given_n2_information_not_in_relation_data_when_relation_changed_then_n2_information_available_event_is_not_emitted(  # noqa: E501
         self,
-        patch_on_n2_information_available,
     ):
-        relation_id = self._create_relation(remote_app_name=self.remote_app_name)
-
-        relation_data = {}
-        self.harness.update_relation_data(
-            relation_id=relation_id, app_or_unit=self.remote_app_name, key_values=relation_data
+        fiveg_n2_relation = scenario.Relation(
+            endpoint="fiveg-n2",
+            interface="fiveg_n2",
+        )
+        state_in = scenario.State(
+            leader=True,
+            relations=[fiveg_n2_relation],
         )
 
-        patch_on_n2_information_available.assert_not_called()
+        self.ctx.run(fiveg_n2_relation.changed_event, state_in)
 
-    @patch(
-        f"{CHARM_LIB_PATH}.N2RequirerCharmEvents.n2_information_available",
-    )
+        assert len(self.ctx.emitted_events) == 1
+
     def test_given_invalid_n2_information_in_relation_data_when_relation_changed_then_n2_information_available_event_is_not_emitted(  # noqa: E501
         self,
-        patch_on_n2_information_available,
     ):
-        relation_id = self._create_relation(remote_app_name=self.remote_app_name)
-
-        relation_data = {
-            "amf_ip_address": "192.168.70.132",
-            "amf_hostname": "amf",
-            "amf_port": "invalid_port123",
-        }
-        self.harness.update_relation_data(
-            relation_id=relation_id, app_or_unit=self.remote_app_name, key_values=relation_data
+        fiveg_n2_relation = scenario.Relation(
+            endpoint="fiveg-n2",
+            interface="fiveg_n2",
+            remote_app_data={
+                "amf_ip_address": "1.2.3.4",
+                "amf_hostname": "amf",
+                "amf_port": "invalid_port123",
+            },
+        )
+        state_in = scenario.State(
+            leader=True,
+            relations=[fiveg_n2_relation],
         )
 
-        patch_on_n2_information_available.assert_not_called()
+        self.ctx.run(fiveg_n2_relation.changed_event, state_in)
 
-    def test_given_n2_information_in_relation_data_when_get_amf_ip_address_is_called_then_ip_is_returned(  # noqa: E501
+        assert len(self.ctx.emitted_events) == 1
+
+    def test_given_n2_information_in_relation_data_when_get_n2_information_is_called_then_information_is_returned(  # noqa: E501
         self,
     ):
-        relation_id = self._create_relation(remote_app_name=self.remote_app_name)
-
-        relation_data = {
-            "amf_ip_address": "192.168.70.132",
-            "amf_hostname": "amf",
-            "amf_port": "38412",
-        }
-        self.harness.update_relation_data(
-            relation_id=relation_id, app_or_unit=self.remote_app_name, key_values=relation_data
+        fiveg_n2_relation = scenario.Relation(
+            endpoint="fiveg-n2",
+            interface="fiveg_n2",
+            remote_app_data={
+                "amf_ip_address": "1.2.3.4",
+                "amf_hostname": "amf",
+                "amf_port": "38412",
+            },
+        )
+        state_in = scenario.State(
+            leader=True,
+            relations=[fiveg_n2_relation],
+        )
+        action = scenario.Action(
+            name="get-n2-information",
         )
 
-        amf_ip_address = self.harness.charm.n2_requirer.amf_ip_address
-        self.assertEqual(amf_ip_address, "192.168.70.132")
+        action_output = self.ctx.run_action(action, state_in)
 
-    def test_given_n2_information_in_relation_data_when_get_amf_hostname_is_called_then_host_is_returned(  # noqa: E501
-        self,
-    ):
-        relation_id = self._create_relation(remote_app_name=self.remote_app_name)
-
-        relation_data = {
-            "amf_ip_address": "192.168.70.132",
-            "amf_hostname": "amf",
-            "amf_port": "38412",
+        assert action_output.success is True
+        assert action_output.results
+        assert action_output.results == {
+            "amf-ip-address": "1.2.3.4",
+            "amf-hostname": "amf",
+            "amf-port": "38412",
         }
-        self.harness.update_relation_data(
-            relation_id=relation_id, app_or_unit=self.remote_app_name, key_values=relation_data
-        )
-
-        amf_hostname = self.harness.charm.n2_requirer.amf_hostname
-        self.assertEqual(amf_hostname, "amf")
-
-    def test_given_n2_information_in_relation_data_when_get_amf_port_is_called_then_port_is_returned(  # noqa: E501
-        self,
-    ):
-        relation_id = self._create_relation(remote_app_name=self.remote_app_name)
-
-        relation_data = {
-            "amf_ip_address": "192.168.70.132",
-            "amf_hostname": "amf",
-            "amf_port": "38412",
-        }
-        self.harness.update_relation_data(
-            relation_id=relation_id, app_or_unit=self.remote_app_name, key_values=relation_data
-        )
-
-        amf_port = self.harness.charm.n2_requirer.amf_port
-        self.assertEqual(amf_port, 38412)
-
-    def test_given_n2_information_is_changed_when_get_amf_hostname_is_called_then_new_host_is_returned(  # noqa: E501
-        self,
-    ):
-        relation_id = self._create_relation(remote_app_name=self.remote_app_name)
-
-        relation_data = {
-            "amf_ip_address": "192.168.70.132",
-            "amf_hostname": "amf",
-            "amf_port": "38412",
-        }
-        self.harness.update_relation_data(
-            relation_id=relation_id, app_or_unit=self.remote_app_name, key_values=relation_data
-        )
-
-        relation_data = {
-            "amf_ip_address": "192.168.70.132",
-            "amf_hostname": "amf2",
-            "amf_port": "38412",
-        }
-        self.harness.update_relation_data(
-            relation_id=relation_id, app_or_unit=self.remote_app_name, key_values=relation_data
-        )
-
-        amf_hostname = self.harness.charm.n2_requirer.amf_hostname
-        self.assertEqual(amf_hostname, "amf2")
-
-    def test_given_n2_information_is_changed_when_get_amf_port_is_called_then_new_port_is_returned(  # noqa: E501
-        self,
-    ):
-        relation_id = self._create_relation(remote_app_name=self.remote_app_name)
-        relation_data = {
-            "amf_ip_address": "192.168.70.132",
-            "amf_hostname": "amf",
-            "amf_port": "38412",
-        }
-        self.harness.update_relation_data(
-            relation_id=relation_id, app_or_unit=self.remote_app_name, key_values=relation_data
-        )
-
-        relation_data = {
-            "amf_ip_address": "192.168.70.132",
-            "amf_hostname": "amf",
-            "amf_port": "38413",
-        }
-        self.harness.update_relation_data(
-            relation_id=relation_id, app_or_unit=self.remote_app_name, key_values=relation_data
-        )
-
-        amf_port = self.harness.charm.n2_requirer.amf_port
-        self.assertEqual(amf_port, 38413)
--- tests/unit/test_charm_remove.py
+++ tests/unit/test_charm_remove.py
@@ -1,34 +1,13 @@
 # Copyright 2024 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-from unittest.mock import patch
 
-import pytest
 import scenario
 
-from charm import AMFOperatorCharm
-from k8s_service import K8sService
+from tests.unit.fixtures import AMFUnitTestFixtures
 
 
-class TestCharmRemove:
-    patcher_k8s_service = patch("charm.K8sService", autospec=K8sService)
-
-    @pytest.fixture(autouse=True)
-    def context(self):
-        self.ctx = scenario.Context(
-            charm_type=AMFOperatorCharm,
-        )
-
-    @pytest.fixture(autouse=True)
-    def setup(self, request):
-        self.mock_k8s_service = TestCharmRemove.patcher_k8s_service.start().return_value
-        yield
-        request.addfinalizer(self.teardown)
-
-    @staticmethod
-    def teardown() -> None:
-        patch.stopall()
-
+class TestCharmRemove(AMFUnitTestFixtures):
     def test_given_k8s_service_created_when_remove_then_external_service_is_deleted(self):
         container = scenario.Container(
             name="amf",
