e6030690907bd08585230d4e824b7433a93ed3a1
Ghislain Bourgeois <ghislain.bourgeois@canonical.com>
2024-10-01 16:11:25 -0400

    chore: Refactor to follow spec CC006

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,36 +6,18 @@ output "app_name" {
   value       = juju_application.amf.name
 }
 
-# Required integration endpoints
-
-output "fiveg_nrf_endpoint" {
-  description = "Name of the endpoint used to integrate with the NRF."
-  value       = "fiveg_nrf"
+output "requires" {
+  value = {
+    fiveg_nrf = "fiveg_nrf"
+    sdcore_config = "sdcore_config"
+    certificates = "certificates"
+    logging = "logging"
+  }
 }
 
-output "sdcore_config_endpoint" {
-  description = "Name of the endpoint used to integrate with the NMS."
-  value       = "sdcore_config"
-}
-
-output "certificates_endpoint" {
-  description = "Name of the endpoint used to integrate with the TLS certificates provider."
-  value       = "certificates"
-}
-
-output "logging_endpoint" {
-  description = "Name of the endpoint used to integrate with the Logging provider."
-  value       = "logging"
-}
-
-# Provided integration endpoints
-
-output "metrics_endpoint" {
-  description = "Exposes the Prometheus metrics endpoint providing telemetry about the AMF instance."
-  value       = "metrics-endpoint"
-}
-
-output "fiveg_n2_endpoint" {
-  description = "Name of the endpoint used to provide information on connectivity to the N2 plane."
-  value       = "fiveg-n2"
+output "provides" {
+  value = {
+    metrics = "metrics-endpoint"
+    fiveg_n2 = "fiveg-n2"
+  }
 }
